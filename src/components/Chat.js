2ea2cbd908cef8a879cd1850a19dab400601c6a8
pablofolgar <pablofolgar@yahoo.com>
2017-06-06 22:52:26 -0300

    Chat Harcoded Version

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -4,16 +4,53 @@ import {
     Text,
 } from 'react-native';
 
+import {GiftedChat} from 'react-native-gifted-chat';
+
 class Chat extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {messages: []};
+        this.onSend = this.onSend.bind(this);
+    }
+
+    componentWillMount() {
+        this.setState({
+          messages: [
+            {
+              _id: 1,
+              text: 'Hello developer',
+              createdAt: new Date(Date.UTC(2016, 7, 30, 17, 20, 0)),
+              user: {
+                _id: 2,
+                name: 'React Native',
+                avatar: 'https://facebook.github.io/react/img/logo_og.png',
+              },
+            },
+          ],
+        });
+      }
+
+
     render(){
         return(
-            <View>
-                <Text>
-                    Hello {this.props.name}
-                </Text>
-            </View>
+            <GiftedChat
+                    messages={this.state.messages}
+                    onSend={this.onSend}
+                    user={{
+                      _id: 1,
+                    }}
+             />
         );
     }
+
+    onSend(messages = []) {
+        this.setState((previousState) => {
+          return {
+            messages: GiftedChat.append(previousState.messages, messages),
+          };
+        });
+      }
+
 }
 
 Chat.defaultProps = {
